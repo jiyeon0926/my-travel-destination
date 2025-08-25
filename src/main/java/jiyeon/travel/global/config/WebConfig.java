4373037025e5eb02d9e265f79e833dffb9779b6c
jiyeon0926 <yeoni9094@naver.com>
2025-08-26 01:36:53 +0900

    feat: SecurityConfig, WebConfig
    - BCrypt 기반의 비밀번호 암호화
    - 화이트리스트

---
+++ src/main/java/jiyeon/travel/global/config/WebConfig.java
@@ -0,0 +1,30 @@
+package jiyeon.travel.global.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig {
+
+    private static final String[] WHITE_LIST = {"/api/auth/signup", "/api/auth/login", "/api/blogs/search"};
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(WHITE_LIST).permitAll()
+                                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.ERROR).permitAll()
+                                .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
