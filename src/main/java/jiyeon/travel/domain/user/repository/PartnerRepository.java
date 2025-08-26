a92eb837e527f76d3e7c8efd5c11b92d7e2ba099
jiyeon0926 <yeoni9094@naver.com>
2025-08-27 02:04:26 +0900

    refactor: 업체 등록
    - 이메일 및 사업장번호 중복 예외처리
    
    #9

--- src/main/java/jiyeon/travel/domain/user/repository/PartnerRepository.java
+++ src/main/java/jiyeon/travel/domain/user/repository/PartnerRepository.java
@@ -4,6 +4,10 @@ import jiyeon.travel.domain.user.entity.Partner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PartnerRepository extends JpaRepository<Partner, Long> {
+
+    Optional<Partner> findByBusinessNumber(String businessNumber);
 }
--- src/main/java/jiyeon/travel/domain/user/service/AdminService.java
+++ src/main/java/jiyeon/travel/domain/user/service/AdminService.java
@@ -7,9 +7,11 @@ import jiyeon.travel.domain.user.repository.PartnerRepository;
 import jiyeon.travel.domain.user.repository.UserRepository;
 import jiyeon.travel.global.common.enums.UserRole;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +23,16 @@ public class AdminService {
 
     @Transactional
     public PartnerSignupResDto signupPartner(String email, String password, String name, String businessNumber, String address, String phone) {
+        userRepository.findByEmail(email)
+                .ifPresent(user -> {
+                    throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
+                });
+
+        partnerRepository.findByBusinessNumber(businessNumber)
+                .ifPresent(partner -> {
+                    throw new ResponseStatusException(HttpStatus.CONFLICT, "동일한 사업장번호가 존재합니다.");
+                });
+
         String encodedPassword = passwordEncoder.encode(password);
         User user = new User(email, encodedPassword, name, phone, UserRole.PARTNER);
         User savedUser = userRepository.save(user);
