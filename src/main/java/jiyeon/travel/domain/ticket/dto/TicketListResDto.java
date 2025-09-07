64326f5c01a0af8b89c5ecf079b13234aefad47e
jiyeon0926 <yeoni9094@naver.com>
2025-09-08 06:23:12 +0900

    refactor: TicketListResDto
    - 생성자 대신 @RequiredArgsConstructor 사용
    - @RequiredArgsConstructor: final 붙은 필드를 대상으로 생성자를 자동 생성

--- src/main/java/jiyeon/travel/domain/ticket/dto/TicketListResDto.java
+++ src/main/java/jiyeon/travel/domain/ticket/dto/TicketListResDto.java
@@ -1,17 +1,14 @@
 package jiyeon.travel.domain.ticket.dto;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@RequiredArgsConstructor
 public class TicketListResDto {
 
     private final Long total;
     private final List<TicketSimpleResDto> tickets;
-
-    public TicketListResDto(Long total, List<TicketSimpleResDto> tickets) {
-        this.total = total;
-        this.tickets = tickets;
-    }
 }
