586c4068b97c02e4a743cb91e3fd495571f5bfec
jiyeon0926 <yeoni9094@naver.com>
2025-10-27 08:12:11 +0900

    fix: Swagger description
    - \n 줄바꿈 및 a 태그 적용 안됨
    - Swagger는 Markdown을 적용해야 함
    - contact 제거

--- src/main/java/jiyeon/travel/global/config/SwaggerConfig.java
+++ src/main/java/jiyeon/travel/global/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package jiyeon.travel.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -28,15 +27,9 @@ public class SwaggerConfig {
     }
 
     private Info apiInfo() {
-        Contact contact = new Contact();
-        contact.name("김지연")
-                .url("https://github.com/jiyeon0926")
-                .email("wldus2499@naver.com");
-
         return new Info()
                 .title("My Travel Destination API")
                 .summary("사용자들이 작성한 여행 블로그를 한 곳에서 확인하고, 나의 다음 여행지를 발견하는 서비스")
-                .description("GitHub: <a href='https://github.com/jiyeon0926/my-travel-destination'></a>")
-                .contact(contact);
+                .description("[GitHub README를 참고해주세요.](https://github.com/jiyeon0926/my-travel-destination)");
     }
 }
